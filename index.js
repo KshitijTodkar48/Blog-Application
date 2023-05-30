b2079fba4ee73ab4991ec6f3ec35248b7497be88
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-05-31 01:27:36 +0530

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express") ;
+const mongoose = require("mongoose") ;
 const path = require("path") ;
+const cookieParser = require("cookie-parser") ;
 const app = express() ;
 const PORT = 8000 ;
+const userRoutes = require("./routes/user") ;
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+mongoose.connect("mongodb://127.0.0.1:27017/blog-hub")
+.then( (e) => console.log("MongoDB connected.")) ;
 
 app.set("view engine" , "ejs") ;
 app.set("views" , path.resolve("./views"))
 
+app.use(express.urlencoded({ extended: false })) ;
+app.use(cookieParser()) ;
+app.use(checkForAuthenticationCookie("token")) ;
+
 app.get("/", (req , res) => {
-    res.render("home") ;
+    res.render("home" , {
+        user: req.user ,
+    }) ;
 })
+
+app.use("/user" , userRoutes) ;
 
 app.listen(PORT , () => { console.log(`Server started at PORT : ${PORT}`) }) ;
---
+++ services/authentication.js
@@ -0,0 +1,23 @@
+const JWT = require("jsonwebtoken") ;
+const secret = "$Kshitij#123$48" ;
+
+function createTokenForUser(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        profilImageURL: user.profileImageURL,
+        role: user.role,
+    };
+    const token = JWT.sign(payload , secret) ;
+    return token ;
+}
+
+function validateToken(token) {
+    const payload = JWT.verify(token , secret) ;
+    return payload ;
+}
+
+module.exports = {
+    createTokenForUser ,
+    validateToken ,
+}
