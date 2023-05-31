f4905e386f598d395c64a8e594d52e942799d82f
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-06-01 00:14:28 +0530

    third commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser") ;
 const app = express() ;
 const PORT = 8000 ;
 const userRoutes = require("./routes/user") ;
+const blogRoutes = require("./routes/blog") ;
+const Blog = require("./models/blog") ;
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 mongoose.connect("mongodb://127.0.0.1:27017/blog-hub")
@@ -16,13 +18,17 @@ app.set("views" , path.resolve("./views"))
 app.use(express.urlencoded({ extended: false })) ;
 app.use(cookieParser()) ;
 app.use(checkForAuthenticationCookie("token")) ;
+app.use(express.static(path.resolve("./public"))) ;
 
-app.get("/", (req , res) => {
+app.get("/", async (req , res) => {
+    const allBlogs = await Blog.find({}) ;
     res.render("home" , {
         user: req.user ,
+        blogs: allBlogs ,
     }) ;
 })
 
 app.use("/user" , userRoutes) ;
+app.use("/blog" , blogRoutes) ;
 
 app.listen(PORT , () => { console.log(`Server started at PORT : ${PORT}`) }) ;
---
+++ routes/blog.js
@@ -0,0 +1,37 @@
+const { Router } = require("express") ;
+const multer = require("multer") ;
+const path = require("path") ;
+const Blog = require("../models/blog") ;
+
+const router = Router() ;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`)) ;
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}` ;
+      cb(null, fileName) ;
+    }
+  })
+
+  const upload = multer({ storage: storage }) ;
+
+router.get("/add-new", (req , res) => {
+    res.render("addBlog" , {
+        user: req.user
+    }) ;
+})
+
+router.post("/", upload.single("coverImage"), async (req , res) => {
+    const { title , body } = req.body ;
+    const blog = await Blog.create({
+        title,
+        body,
+        createdBy: req.user._id,
+        coverImageURL: `/uploads/${req.file.filename}`,
+    });
+    res.redirect(`/blog/${blog._id}`) ;
+})
+
+module.exports = router ;
