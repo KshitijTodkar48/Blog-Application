3a567e0ca406ebafe5ecbc98a84d7579f4b3a4e9
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-06-02 01:38:22 +0530

    added comment-section

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const { Router } = require("express") ;
 const multer = require("multer") ;
 const path = require("path") ;
 const Blog = require("../models/blog") ;
+const Comment = require("../models/comment") ;
 
 const router = Router() ;
 
@@ -15,7 +16,7 @@ const storage = multer.diskStorage({
     }
   })
 
-  const upload = multer({ storage: storage }) ;
+const upload = multer({ storage: storage }) ;
 
 router.get("/add-new", (req , res) => {
     res.render("addBlog" , {
@@ -32,6 +33,25 @@ router.post("/", upload.single("coverImage"), async (req , res) => {
         coverImageURL: `/uploads/${req.file.filename}`,
     });
     res.redirect(`/blog/${blog._id}`) ;
+})
+
+router.get("/:id", async (req , res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy") ;
+  const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy") ;
+  return res.render("blog",{
+    user: req.user,
+    blog,
+    comments
+  });
+})
+
+router.post("/comment/:blogId" , async (req , res) => {
+  await Comment.create({
+    content: req.body.content ,
+    blogId: req.params.blogId ,
+    createdBy: req.user._id ,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`) ;
 })
 
 module.exports = router ;
